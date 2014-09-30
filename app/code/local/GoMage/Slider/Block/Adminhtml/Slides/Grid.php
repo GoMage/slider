b6b101501a250ec4ac70e27d1db330889f4aae2d
Andrey Zac <az@stepcart.com>
2014-09-30 14:15:31 +0300

    Copyright

--- app/code/local/GoMage/Slider/Block/Adminhtml/Slides/Grid.php
+++ app/code/local/GoMage/Slider/Block/Adminhtml/Slides/Grid.php
@@ -3,7 +3,7 @@
  * GoMage Slider Extension
  *
  * @category     Extension
- * @copyright    Copyright (c) 2010-2012 GoMage (http://www.gomage.com)
+ * @copyright    Copyright (c) 2010-2014 GoMage (http://www.gomage.com)
  * @author       GoMage
  * @license      http://www.gomage.com/license-agreement/  Single domain license
  * @terms of use http://www.gomage.com/terms-of-use
